e794e56f014b071c543d3df0a44f470df3c650dd
Anup K Jana <janaanup7872@gmail.com>
2024-10-26 20:52:17 +0530

    all interview behavioral pages

---
+++ frontend/src/pages/Dashboard-Pages/Interview-prep/Behavioral-Pages/BehavioralInterviewPrepPages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BehavioralInterviewPrepPages = () => {
+  return (
+    <div>BehavioralInterviewPrepPages</div>
+  )
+}
+
+export default BehavioralInterviewPrepPages
